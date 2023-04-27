35f27fb02c937d960f1caab01c609801054761d0
ThaksinCK <thaksin.ckn@gmail.com>
2023-04-27 22:02:44 +0700

    add localDatabase

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const _ = require('lodash');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -13,15 +14,27 @@ const port = 3000;
 const posts = [];
 const content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
+mongoose.connect('mongodb://127.0.0.1:27017/blogContent');
+
+const blogSchema = mongoose.Schema({
+    title : String,
+    content : String
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
 app.get('/', (req, res)=>{
-    res.render('home', {'startingContent': content, 'posts' : posts});
+    Blog.find({}).then((value)=>{
+        res.render('home', {'startingContent': content, 'posts' : value});
+    })
 })
 
 app.get('/:mainPath' , (req, res)=>{
 	var mainPath = req.params.mainPath;
-    console.log(mainPath);
 	if(mainPath === 'home'){
-        res.render('home', {'startingContent': content, 'posts' : posts});
+        Blog.find({}).then((value)=>{
+            res.render('home', {'startingContent': content, 'posts' : value});
+        })
 	}
     else if(mainPath === 'about'){
         res.render('about', {'startingContent' : content});
@@ -38,27 +51,18 @@ app.get('/:mainPath' , (req, res)=>{
 })
 
 app.get('/posts/:titleName', (req, res)=>{
-    var titleid = _.lowerCase((req.params.titleName));
-    posts.forEach((element)=>{
-		console.log(titleid);
-		console.log(_.lowerCase(element.title));
-        if(_.lowerCase(element.title) === titleid){
-			res.render('post', {'title' : element.title, 'titleId' : titleid, 'content' : element.content});
-		}
-        else{
-			res.render('error');
-        }
+    var titleid = req.params.titleName;
+    Blog.find({_id : titleid}).then((value)=>{
+        res.render('post', {'title' : value[0].title, 'content' : value[0].content});
     })
-    
 })
 
 app.post('/compose', (req, res)=>{
-    var post = {
+    const blog = new Blog({
         title : req.body.title,
-        content : req.body.postContent,
-		titleid : _.lowerCase(req.body.title)
-    };
-    posts.push(post);
+        content : req.body.postContent
+    });
+    blog.save().then(()=> console.log("Save"));
     res.redirect('home');
 })
 
